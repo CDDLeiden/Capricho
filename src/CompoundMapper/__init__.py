bd4349d2e1e585321877e57f773fa04418623de6
David-Araripe <david.araripe17@gmail.com>
2024-04-08 10:47:41 +0200

    add setuptools_scm for automatic versioning

--- src/CompoundMapper/__init__.py
+++ src/CompoundMapper/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.0.1"
-
-# TODO: add logger functionality;
-# TODO: add functions to aggregate the data;
 # TODO: add workflow function to run all functions for a set of molecules (SMILES;Inchikey...);
+
+
+from ._version_helper import get_version
+
+__version__ = get_version()
