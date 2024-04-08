41d1752863938bd6d5595c3a739213132f079d15
David-Araripe <david.araripe17@gmail.com>
2024-04-08 10:48:14 +0200

    add module to configure logger

---
+++ src/CompoundMapper/log_config.py
@@ -0,0 +1,19 @@
+"""Configure luguru logger for the project."""
+from loguru import logger
+import sys
+
+def setup_logger(level="INFO"):
+    """Initialize the logger with a default log level and format"""
+    colorful_format = (
+        "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | "
+        "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+        "<level>{message}</level>"
+    )
+    logger.remove()
+    logger.add(sys.stderr, format=colorful_format, level=level, colorize=True)
+
+def set_log_level(level):
+    """Update the logger's level."""
+    setup_logger(level=level)
+
+setup_logger()  # `logger` is configured whenever it gets imported from this module
