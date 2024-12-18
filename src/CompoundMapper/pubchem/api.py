7a75469d9eccbadd09af593eb9fd0e27277d314e
David-Araripe <david.araripe17@gmail.com>
2024-12-18 14:13:38 +0100

    separate internal pubchem functions for further clarity. Main methods should be within api, using methods from `core.py`

--- src/CompoundMapper/pubchem/api.py
+++ src/CompoundMapper/pubchem/api.py
@@ -1,64 +1,11 @@
 import numpy as np
 import pandas as pd
 from chemFilters.chem.standardizers import ChemStandardizer
-from joblib import Parallel, delayed
-from tqdm import tqdm
 
-try:
-    from pubchempy import Compound, get_compounds
-except ImportError:
-    raise ImportError("pubchempy is required for this module. To install: pip install pubchempy")
-
-from ..chembl.rate_limit import rate_limit
 from ..cli.fp_utils import calculate_mixed_FPs
 from ..core.smiles_utils import clean_mixtures
 from ..core.stats_make import repeated_indices_from_array_series
-
-
-@rate_limit(4)  # 4 calls per second not to exceed the PubChem API rate limit (5/sec)
-def get_compound_by(cpd_input, input_type: str = "name") -> list[Compound | None]:
-    """Get a compound by a specific identifier using pubchempy.
-
-    Args:
-        cpd_input: input to fetch
-        input_type: type of input. Defaults to "name".
-
-    Raises:
-        ValueError: if invalid compound_input is provided
-
-    Returns:
-        List[Compound: PubChem Compound object]
-    """
-    supported_inputs = ["name", "smiles", "sdf", "inchi", "inchikey", "formula"]
-    if input_type not in supported_inputs:
-        raise ValueError("Invalid compound_input")
-    return get_compounds(cpd_input, input_type, list_return="flat")
-
-
-def get_multiple_compounds(cpd_list, input_type: str = "name", n_jobs=4) -> list[list[Compound | None]]:
-    """
-    Fetch multiple compounds in parallel using joblib.
-
-    Args:
-        cpd_list: List of compound identifiers to fetch
-        input_type: Type of input (name, smiles, etc.)
-        n_jobs: Number of parallel jobs (4 is advised as max here)
-
-    Returns:
-        list: List of compound objects
-    """
-    results = []
-
-    try:
-        results = Parallel(n_jobs=n_jobs, backend="threading")(
-            delayed(get_compound_by)(compound, input_type) for compound in tqdm(cpd_list)
-        )
-
-        return results
-
-    except Exception as e:
-        print(f"Error in parallel processing: {str(e)}")
-        return []
+from .core import get_multiple_compounds
 
 
 def get_and_curate_multiple_compounds_result(
@@ -102,6 +49,7 @@ def get_and_curate_multiple_compounds_result(
                     "isomeric_smiles": np.nan,
                     "canonical_smiles": np.nan,
                     "iupac_name": np.nan,
+                    "synonyms": np.nan,
                 }
             ]
         )
@@ -120,6 +68,7 @@ def get_and_curate_multiple_compounds_result(
                 "isomeric_smiles": r.isomeric_smiles,
                 "canonical_smiles": r.canonical_smiles,
                 "iupac_name": r.iupac_name,
+                "synonyms": r.synonyms,
             }
             for r in res
         ]
