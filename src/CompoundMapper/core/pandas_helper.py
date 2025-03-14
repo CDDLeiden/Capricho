fix data aggregation to use geometric mean & standard deviation ➡️ correct for aggregating log-transformed values like pChEMBL

src/CompoundMapper/core/pandas_helper.py
@@ -5,7 +5,7 @@ from typing import Union
 
 import numpy as np
 import pandas as pd
-from scipy.stats import median_abs_deviation
+from scipy.stats import gmean, gstd, median_abs_deviation
 
 from ..logger import logger
 
@@ -43,6 +43,25 @@ def get_mad(values) -> Union[float, np.float64]:
         return np.nan
 
 
+def gmedian(values) -> Union[float, np.float64]:
+    """Calculate the median of a list of -log transformed numerical values. If even number
+    of values, return the geometric mean of the two middle values.
+
+    Args:
+        values: List of -log transformed numerical values
+
+    Returns:
+        float: Geometric median of the values, transformed back to the original scale.
+    """
+    if len(values) % 2 != 0:
+        return np.median(values)
+
+    # Even number of elements
+    sorted_values = np.sort(values)
+    mid_index = len(sorted_values) // 2
+    return gmean([values[mid_index - 1], values[mid_index]])
+
+
 def merge_dataframes(dfs, id_cols) -> pd.DataFrame:
     """
     Merge a list of DataFrames based on id_cols. Useful reference for merges:
@@ -78,7 +97,7 @@ def apply_func_grpd(grpd, func: callable, idcols: list, *cols: list) -> pd.DataF
     return pd.concat(results, ignore_index=False, axis=1).reset_index()
 
 
-def assign_stats(df: pd.DataFrame, sep=";", value_col="pchembl_value") -> pd.DataFrame:
+def assign_stats(df: pd.DataFrame, sep=";", value_col="pchembl_value", use_geometric=False) -> pd.DataFrame:
     """Assign statistics to a DataFrame based on a column with multiple values separated by
     a particular separator, e.g. ';'.
 
@@ -86,6 +105,9 @@ def assign_stats(df: pd.DataFrame, sep=";", value_col="pchembl_value") -> pd.Dat
         df: pd.DataFrame to be processed.
         sep: string separating the values. Defaults to ';'.
         value_col: column containing the values to be processed. Defaults to "pchembl_value".
+        use_geometric: if True, treats values as -log[unit] and converts them into the original
+            scale to calculate the statistics. If False, transformation doesn't take place.
+            Defaults to True.
 
     Returns:
         pd.DataFrame: DataFrame with the statistics assigned. as columns:
@@ -93,22 +115,22 @@ def assign_stats(df: pd.DataFrame, sep=";", value_col="pchembl_value") -> pd.Dat
     >>>     f"{value_col}_mean",
     >>>     f"{value_col}_std",
     >>>     f"{value_col}_median",
-    >>>     f"{value_col}_mad",
     >>>     f"{value_col}_counts",
     >>> ]
     """
 
     value_series = df[value_col].astype(str).str.split(sep).apply(lambda x: list(map(float, x)))
-    new_cols = [f"{value_col}{suffix}" for suffix in ["_mean", "_std", "_median", "_mad", "_counts"]]
-    values = [
-        value_series.apply(np.mean),
-        value_series.apply(np.std),
-        value_series.apply(np.median),
-        value_series.apply(get_mad),
-        value_series.apply(lambda x: len(x)),
-    ]
-    for c, v in zip(new_cols, values):
-        df[c] = v
+    new_cols = [f"{value_col}{suffix}" for suffix in ["_mean", "_std", "_median", "_counts"]]
+    if use_geometric:
+        df[new_cols[0]] = value_series.apply(gmean)
+        df[new_cols[1]] = value_series.apply(gstd)
+        df[new_cols[2]] = value_series.apply(lambda x: -np.log10(10 ** (-np.median(x))))
+        df[new_cols[4]] = value_series.apply(len)
+    else:
+        df[new_cols[0]] = value_series.apply(np.mean)
+        df[new_cols[1]] = value_series.apply(np.std)
+        df[new_cols[2]] = value_series.apply(np.median)
+        df[new_cols[4]] = value_series.apply(len)
     return df
 
 