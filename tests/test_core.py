48399cb76a953d7c28c4538f0b9206dbc48414e6
David-Araripe <david.araripe17@gmail.com>
2025-07-14 13:29:51 +0200

    remove test_pandas_helper and add the unit test to test_core.py instead

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 
 from CompoundMapper.core import pandas_helper, smiles_utils, stats_make
+from CompoundMapper.core.pandas_helper import conflicting_duplicates
 
 
 class TestPandasHelper(unittest.TestCase):
@@ -46,6 +47,53 @@ class TestPandasHelper(unittest.TestCase):
         self.assertAlmostEquals(result["value_std"].iloc[1], 0.816497, 6)
         self.assertEqual(result["value_median"].iloc[1], 5.0)
         self.assertEqual(result["value_counts"].iloc[1], 3)
+
+    def test_conflicting_duplicates(self):
+        data = {
+            "A": [
+                1,
+                1,
+                1,
+                2,
+                2,
+            ],
+            "B": [
+                "x",
+                "x",
+                "x",
+                "y",
+                "y",
+            ],
+            "C": [
+                "p",
+                "p",
+                "p",
+                "q",
+                "q",
+            ],
+            "D": [
+                10,  # Same A, B, C (e.g. mol identifiers) but different D (document)
+                10,  # Same as above
+                20,  # Same as above
+                30,  # Same everything -> reported in same document; shouldn't drop
+                30,  # Same as above
+            ],
+            "year": [
+                2020,
+                2020,
+                2021,
+                2021,
+                2021,
+            ],
+        }
+        df = pd.DataFrame(data).sort_values(by="year")
+        mask = conflicting_duplicates(df, key_subset=["A", "B", "C"], diff_subset=["D"])
+        expected_flags = [True, True, True, False, False]
+        true_idxs = np.where(mask)[0]
+        assert np.array_equal(
+            true_idxs, np.array([0, 1, 2])
+        ), "Expected indices with conflicting duplicates do not match."
+        pd.testing.assert_series_equal(mask, pd.Series(expected_flags, index=df.index))
 
 
 class TestSmilesUtils(unittest.TestCase):
